640ae73daf5db584a4926fd3003baae4657a5714
yamaokamiyuu <gc23013@anabuki-net.ne.jp>
2023-09-20 09:33:26 +0900

    add project

---
+++ 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
@@ -0,0 +1,27 @@
+#include "CApplication.h"
+#include "CRectangle.h"
+
+//クラスのstatic変数
+CTexture CApplication::mTexture;
+CCharacterManager CApplication::mCharacterManager;
+
+#define SOUND_BGM "res\\mario.wav" //BGM音声ファイル
+#define SOUND_OVER "res\\mdai.wav" //ゲームオーバー音声ファイル
+
+CCharacterManager* CApplication::CharacterManager()
+{
+	return &mCharacterManager;
+}
+
+CTexture* CApplication::Texture()
+{
+	return &mTexture;
+}
+
+void CApplication::Start()
+{
+}
+
+void CApplication::Update()
+{
+}
