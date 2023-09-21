3907d7edb5fa8a3a657517689806448de88cafdb
yamaokamiyuu <gc23013@anabuki-net.ne.jp>
2023-09-21 12:19:02 +0900

    a

--- 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
+++ 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
@@ -2,6 +2,7 @@
 #include "CRectangle.h"
 //OpenGL
 #include "glut.h"
+#include "CVector.h"
 
 
 //クラスのstatic変数
@@ -27,6 +28,17 @@ void CApplication::Start()
 
 void CApplication::Update()
 {
+	//頂点１，頂点２，頂点３，法線データの作成
+	CVector v0, v1, v2, n;
+	//法線を上向きで設定する
+	n.Set(0.0f, 1.0f, 0.0f);
+	//頂点１の座標を設定する
+	v0.Set(0.0f, 0.0f, 0.5f);
+	//頂点２の座標を設定する
+	v1.Set(1.0f, 0.0f, 0.0f);
+	//頂点３の座標を設定する
+	v2.Set(0.0f, 0.0f, -0.5f);
+
 	//視点の設定
 	//gluLookAt(視点X,視点Y,視点Z,中心X,中心Y,中心Z,上向X,上向Y,上向Z)
 	gluLookAt(1.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
@@ -38,24 +50,35 @@ void CApplication::Update()
 
 	//法線(面の向き)の設定
 	//glNormal3f(X座標,Y座標,Z座標)
-	glNormal3f(0.0f, 1.0f, 0.0f);
+	glNormal3f(n.X(),n.Y(),n.Z());
 
 	//頂点座標の設定
 	//glVertex3f(X座標,Y座標,Z座標)
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, -0.5f);
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
-	//面の向きはZ軸方向
-	glNormal3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-0.5f, 0.0f, 0.0f);
+	//法線と頂点の設定
+	n.Set(0.0f, 0.0f, 1.0f);
+	v0.Set(0.5f, 0.0f, 0.0f);
+	v1.Set(0.0f, 1.0f, 0.0f);
+	v2.Set(-0.5f, 0.0f, 0.0f);
+	//三角形２の描画
+	glNormal3f(n.X(), n.Y(), n.Z());
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
-	glNormal3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, -0.5f, 0.0f);
+	//法線と頂点の設定
+	n.Set(1.0f, 0.0f, 0.0f);
+	v0.Set(0.0f, 0.5f, 0.0f);
+	v1.Set(0.0f, 0.0f, 1.0f);
+	v2.Set(0.0f, -0.5f, 0.0f);
+	//三角形３の描画
+	glNormal3f(n.X(), n.Y(), n.Z());
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
 	//描画終了
 	glEnd();
