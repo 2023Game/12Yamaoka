413b671bf89fe3956fdff8d178e6a4eca4e31ff0
yamaokamiyuu <gc23013@anabuki-net.ne.jp>
2024-04-17 12:28:32 +0900

    2

--- 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CModelX.h
+++ 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CModelX.h
@@ -15,9 +15,13 @@ public:
 	CModelX();
 	//ファイル読み込み
 	void Load(char* file);
+	//単語の取り出し
+	char* GetToken();
 private:
 	char* mpPointer; //読み込み位置
 	char mToken[1024]; //取り出した単語の領域
+	//cが区切り文字ならtrueを返す
+	bool IsDelimiter(char c);
 };
 
 #endif
