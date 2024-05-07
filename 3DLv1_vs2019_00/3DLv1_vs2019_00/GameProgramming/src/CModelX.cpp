136d3003657cd56e5663f3038a131bad329d7f58
yamaokamiyuu <gc23013@anabuki-net.ne.jp>
2024-05-07 10:37:28 +0900

    7

--- 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
+++ 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
@@ -257,6 +257,8 @@ CMesh::CMesh()
 	, mpVertex(nullptr)
 	, mFaceNum(0)
 	, mpVertexIndex(nullptr)
+	, mNormalNum(0)
+	, mpNormal(nullptr)
 {
 }
 
@@ -265,6 +267,7 @@ CMesh::~CMesh()
 {
 	SAFE_DELETE_ARRAY(mpVertex);
 	SAFE_DELETE_ARRAY(mpVertexIndex);
+	SAFE_DELETE_ARRAY(mpNormal);
 }
 
 /*
@@ -301,18 +304,62 @@ void CMesh::Init(CModelX* model)
 		mpVertexIndex[i + 1] = atoi(model->GetToken());
 		mpVertexIndex[i + 2] = atoi(model->GetToken());
 	}
+	
+	model->GetToken(); //MeshNormals
+	if (strcmp(model->Token(), "MeshNormals") == 0)
+	{
+		model->GetToken(); // {
+		//法線データ数を取得
+		mNormalNum = atoi(model->GetToken());
+		//法線データを配列に取り込む
+		CVector* pNormal = new CVector[mNormalNum];
+		for (int i = 0; i < mNormalNum; i++)
+		{
+			pNormal[i].X(atof(model->GetToken()));
+			pNormal[i].Y(atof(model->GetToken()));
+			pNormal[i].Z(atof(model->GetToken()));
+		}
+		//法線数＝面数×３
+		mNormalNum = atoi(model->GetToken()) * 3; //FaceNum
+		int ni;
+		//頂点毎に法線データを設定する
+		mpNormal = new CVector[mNormalNum];
+		for (int i = 0; i < mNormalNum; i += 3)
+		{
+			model->GetToken(); // 3
+			ni = atoi(model->GetToken());
+			mpNormal[i] = pNormal[ni];
+
+			ni = atoi(model->GetToken());
+			mpNormal[i + 1] = pNormal[ni];
+
+			ni = atoi(model->GetToken());
+			mpNormal[i + 2] = pNormal[ni];
+		}
+		delete[] pNormal;
+		model->GetToken(); // }
+	} // End of MeshNormals
+
 #ifdef _DEBUG
+	/*
 	//デバッグ時に、読み込んだ頂点数と頂点座用をコンソール出力する
 	printf("VertexNum:%d\n", mVertexNum);
 	for (int i = 0; i < mVertexNum; i++)
 	{
-		printf("%10f %10f %10f\n",mpVertex[i].X(), mpVertex[i].Y(), mpVertex[i].Z());
+		printf("%10f %10f %10f\n", mpVertex[i].X(), mpVertex[i].Y(), mpVertex[i].Z());
 	}
 	//デバッグ時に、読み込んだ面数と面を構成する頂点番号をコンソール出力する
 	printf("FaceNum:%d\n", mFaceNum);
 	for (int i = 0; i < mFaceNum * 3; i += 3)
 	{
 		printf("%2d %2d %2d\n", mpVertexIndex[i], mpVertexIndex[i + 1], mpVertexIndex[i + 2]);
+	}
+	*/
+	//デバッグ時に、法線数と法線ベクトル84個を頂点の順にコンソール出力する
+	printf("NormalNum:%d\n", mNormalNum);
+	for (int i = 0; i < mNormalNum; i++)
+	{
+		printf("%10f %10f %10f\n", mpNormal[i].X(), mpNormal[i].Y(), mpNormal[i].Z());
 	}
 #endif
 }
