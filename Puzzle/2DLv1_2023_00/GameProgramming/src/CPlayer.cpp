966a5cc6e952db8f040cf12b26919bb40457d6ed
yamaokamiyuu <gc23013@anabuki-net.ne.jp>
2024-05-20 15:15:20 +0900

    a

--- Puzzle/2DLv1_2023_00/GameProgramming/src/CPlayer.cpp
+++ Puzzle/2DLv1_2023_00/GameProgramming/src/CPlayer.cpp
@@ -41,14 +41,14 @@ void CPlayer::Update()
 
 	if (isMoving)
 	{
-		if (GetAsyncKeyState(VK_UP))
-		{
-			float y = Y() + 4.0f;
-			Y(y);
-		}
 		if (GetAsyncKeyState(VK_DOWN))
 		{
 			float y = Y() - 4.0f;
+			Y(y);
+		}
+		if (GetAsyncKeyState(VK_UP))
+		{
+			float y = Y() + 4.0f;
 			Y(y);
 		}
 
@@ -61,6 +61,42 @@ void CPlayer::Update()
 		{
 			float x = X() + 4.0f;
 			X(x);
+		}
+	}
+
+	static int lastMouseY = 0;
+	static int lastMouseX = 0;
+
+	POINT mousePos;
+	if (GetCursorPos(&mousePos))
+	{
+		ScreenToClient(HWND_BOTTOM, &mousePos);
+		if (GetAsyncKeyState(VK_LBUTTON))
+		{
+			if (mousePos.y < lastMouseY)
+			{
+				float y = Y() + 4.0f;
+				Y(y);
+			}
+			else if (mousePos.y > lastMouseY)
+			{
+				float y = Y() - 4.0f;
+				Y(y);
+			}
+			lastMouseY = mousePos.y;
+
+			if (mousePos.x < lastMouseX)
+			{
+				float x = X() - 4.0f;
+				X(x);
+			}
+			else if (mousePos.x > lastMouseX)
+			{
+				float x = X() + 4.0f;
+				X(x);
+			}
+			lastMouseX = mousePos.x;
+			
 		}
 	}
 }
