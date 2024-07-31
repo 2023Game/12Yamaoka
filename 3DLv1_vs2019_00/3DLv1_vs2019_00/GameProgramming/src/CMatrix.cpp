2952fda5556dae8286c54cb0786c3d2f1d3ccd65
yamaokamiyuu <gc23013@anabuki-net.ne.jp>
2024-07-31 12:18:55 +0900

    27

--- 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CMatrix.cpp
+++ 3DLv1_vs2019_00/3DLv1_vs2019_00/GameProgramming/src/CMatrix.cpp
@@ -197,7 +197,7 @@ CMatrix CMatrix::Quaternion(float x, float y, float z, float w)
 	mM[0][2] = 2 * x * z + 2 * w * y;
 	mM[0][3] = 0;
 	mM[1][0] = 2 * x * y + 2 * w * z;
-	mM[1][1] = -x * x + y * y + z * z + w * w;
+	mM[1][1] = -x * x + y * y - z * z + w * w;
 	mM[1][2] = 2 * y * z - 2 * w * x;
 	mM[1][3] = 0;
 	mM[2][0] = 2 * x * z - 2 * w * y;
